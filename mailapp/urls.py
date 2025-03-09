e7cb15ea2c8bf090df2ddf71084a98fb373f0c67
Alik <alik220201@gmail.com>
2025-03-09 21:48:00 +0700

    add tamplates and urls

--- mailapp/urls.py
+++ mailapp/urls.py
@@ -5,4 +5,8 @@ from mailapp.apps import MailappConfig
 app_name = MailappConfig.name
 
 urlpatterns = [
-    ]
+     path('', views.RecipientMailListViews.as_view(), name='recipient_mail_list'),
+     path('create/', views.RecipientMailCreateViews.as_view(), name='recipient_mail_create'),
+     path('<int:pk>/update/', views.RecipientMailUpdateViews.as_view(), name='recipient_mail_update'),
+     path('<int:pk>/delete/', views.RecipientMailDeleteViews.as_view(), name='recipient_mail_delete'),
+     ]
--- mailapp/views.py
+++ mailapp/views.py
@@ -1,3 +1,39 @@
-from django.shortcuts import render
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
+from django.urls import reverse_lazy
 
-# Create your views here.
+from .models import RecipientMail
+
+
+class RecipientMailListViews(ListView):
+    model = RecipientMail
+    template_name = 'mailapp/recipient_mail_list.html'
+    context_object_name = 'recipient_mails'
+    paginate_by = 10
+    ordering = ['full_name']
+
+
+class RecipientMailDetailViews(DetailView):
+    model = RecipientMail
+    template_name = 'mailapp/recipient_mail_detail.html'
+    context_object_name = 'recipient_mail'
+    ordering = ['full_name']
+
+
+class RecipientMailCreateViews(CreateView):
+    model = RecipientMail
+    template_name = "mailapp/recipient_mail_form.html"
+    fields = ['email', 'full_name', 'comment']
+    success_url = reverse_lazy('mailapp:recipient_mail_list')
+
+
+class RecipientMailUpdateViews(UpdateView):
+    model = RecipientMail
+    template_name = "mailapp/recipient_mail_form.html"
+    fields = ['email', 'full_name', 'comment']
+    success_url = reverse_lazy('mailapp:recipient_mail_list')
+
+
+class RecipientMailDeleteViews(DeleteView):
+    model = RecipientMail
+    template_name = 'recipient_mail_delete.html'
+    success_url = reverse_lazy('mailapp:recipient_mail_list')
